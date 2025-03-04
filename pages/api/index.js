1c188d36a13b41cfe45bae62439742c4493aa13c
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2025-03-04 23:49:29 +0300

    ref: Deleted unnecessary files

--- pages/api/index.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method Not Allowed" });
-  }
-  res.send("Hello from Chatrix Api ✨");
-}

74d880dbb6f131378c4dea6f750bce7c72e8f27f
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2025-03-04 23:06:03 +0300

    Husky test

--- components/custom/table.jsx/table-pagination.jsx
+++ components/custom/table.jsx/table-pagination.jsx
@@ -28,7 +28,7 @@ export function DataTablePagination({
   onPageChange,
   onRowsPerPageChange,
 }) {
-  console.log("TEst");
+  console.log("ASdasd");
   return (
     <div className="flex items-center justify-between px-2 mt-3">
       <div className="flex items-center space-x-2">
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,17 @@ const eslintConfig = [
     rules: {
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
+      "no-console": "warn", // Warn when using console.log
+      "no-unused-vars": [
+        "warn",
+        { args: "after-used", varsIgnorePattern: "^_" },
+      ], // Warn for unused variables (except _ prefixed ones)
+      "no-undef": "error", // Prevent usage of undefined variables
+      "unused-imports/no-unused-imports": "warn", // Warn for unused imports
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { args: "after-used", varsIgnorePattern: "^_" },
+      ],
     },
   }),
 ];
